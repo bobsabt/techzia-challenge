eb179aba8950a9bcb8e97c6f7e12ee39943c121e
bobsabt <bodai.barbara@gmail.com>
2022-03-07 17:09:15 +0100

    create blueprint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Dashboard from "./Components/Dashboard";
+import Login from "./Components/Login";
+import Profil from "./Components/Profil";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profil" element={<Profil />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
